adaed620f2ff1928d8ade0851284d7d70d06251a
PedroHS <137302836+PedroHsillv@users.noreply.github.com>
2023-10-25 14:59:47 -0300

    Add files via upload

---
+++ SequenciaNumeros/sequencianumeriaca.php
@@ -0,0 +1,17 @@
+<?php
+// Sequência Par de 0 a 500
+echo "<p>Sequência Par:</p>";
+echo "<p>";
+for ($i = 0; $i <= 500; $i += 2) {
+    echo $i . " "; // Imprime o número par atual seguido de um espaço
+}
+echo "</p>";
+
+// Sequência Ímpar de 1 a 499
+echo "<p>Sequência Ímpar:</p>";
+echo "<p>";
+for ($i = 1; $i <= 499; $i += 2) {
+    echo $i . " "; // Imprime o número ímpar atual seguido de um espaço
+}
+echo "</p>";
+?>
